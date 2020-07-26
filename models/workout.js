ee365ebafe34f20ba20effd45379081419188859
Chris Salgado <musicman785@gmail.com>
2020-07-26 14:14:07 -0700

    Created Models folder and file. Created Db Schema

---
+++ models/workout.js
@@ -0,0 +1,36 @@
+//Create workout schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  type: {
+    type: String,
+    trim: true,
+    required: "Must Enter Exercise Type!",
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: "Must Enter Exercise Name!",
+  },
+  duration: {
+    type: Number,
+    required: "Enter Number Minutes!",
+  },
+  weight: {
+    type: Number,
+    required: [false, "Enter Weight if Doing Weight Training!"],
+  },
+  reps: {
+    type: Number,
+    required: [false, "Enter Number of Reps!"],
+  },
+  sets: {
+    type: Number,
+    required: [false, "Enter Numer of Sets!"],
+  },
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
