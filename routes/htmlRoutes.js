e28d43b43af2e6d730f4d292bbc4f62c92cc8486
Chris Salgado <musicman785@gmail.com>
2020-07-26 16:10:29 -0700

    Added paths in html routes. Diplayed all html files in browser

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const path = require("path");
+
+// HTML Routes
+//
+//=====================================
+router.get("/", (req, res) => {
+  res.sendfile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/exercise", (req, res) => {
+  res.sendfile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+  res.sendfile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -21,7 +22,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 const db = require("./models");
 
 // routes
-// app.use(require("./routes/api.js"));
+// app.use(require("./routes/api.js")(app));
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
