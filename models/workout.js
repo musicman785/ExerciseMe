42e0a1d3d5f71463070eedece0f7dd1534109e62
Chris Salgado <musicman785@gmail.com>
2020-07-29 11:01:12 -0700

    Added virtual property on models

--- models/workout.js
+++ models/workout.js
@@ -2,45 +2,59 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now,
-  },
-  exercises: [
-    {
-      type: {
-        type: String,
-        trim: true,
-        required: "Must Enter Exercise Type!",
-      },
-      name: {
-        type: String,
-        trim: true,
-        required: "Must Enter Exercise Name!",
-      },
-      duration: {
-        type: Number,
-        required: "Enter Number Minutes!",
-      },
-      distance: {
-        type: Number,
-        required: [false, "Enter Distance!"],
-      },
-      weight: {
-        type: Number,
-        required: [false, "Enter Weight if Doing Weight Training!"],
-      },
-      reps: {
-        type: Number,
-        required: [false, "Enter Number of Reps!"],
-      },
-      sets: {
-        type: Number,
-        required: [false, "Enter Numer of Sets!"],
-      },
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
     },
-  ],
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Must Enter Exercise Type!",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Must Enter Exercise Name!",
+        },
+        duration: {
+          type: Number,
+          required: "Enter Number Minutes!",
+        },
+        distance: {
+          type: Number,
+          required: [false, "Enter Distance!"],
+        },
+        weight: {
+          type: Number,
+          required: [false, "Enter Weight if Doing Weight Training!"],
+        },
+        reps: {
+          type: Number,
+          required: [false, "Enter Number of Reps!"],
+        },
+        sets: {
+          type: Number,
+          required: [false, "Enter Numer of Sets!"],
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+WorkoutSchema.virtual("totalDuration").get(function () {
+  let totalDuration = 0;
+  for (let i = 0; i < this.exercises.length; i++) {
+    totalDuration += this.exercises[i].duration;
+  }
+  return totalDuration;
 });
 
 const Workouts = mongoose.model("Workouts", WorkoutSchema);
